eec5b5284bb53ee5f89d5a361e36d7140b2f4f34
xuanlin129 <xuan.lin129@gmail.com>
2025-02-07 17:19:15 +0800

    debug

--- command/oil-price.js
+++ command/oil-price.js
@@ -8,40 +8,42 @@ export default async (event) => {
 
     const oilMsg = oil;
 
-    data
-      .filter((oil) => oils.includes(oil.產品名稱))
-      .forEach((oil) => {
-        const info = {
-          type: 'box',
-          layout: 'baseline',
-          contents: [
-            {
-              type: 'text',
-              text: oil.產品名稱,
-              size: 'lg',
-              flex: 2,
-            },
-            {
-              type: 'text',
-              text: `${oil.參考牌價}`,
-              flex: 1,
-              color: '#ff0000',
-              align: 'center',
-              size: 'lg',
-            },
-            {
-              type: 'text',
-              text: '元/公升',
-              flex: 1,
-              align: 'center',
-              size: 'lg',
-            },
-          ],
-          margin: 'md',
-        };
-        oilMsg.contents.body.contents.push(info);
-        oilMsg.contents.footer.contents[0].contents[1].text = `${oil.牌價生效日期}`;
-      });
+    if (oilMsg.contents.body.contents.length === 1) {
+      data
+        .filter((oil) => oils.includes(oil.產品名稱))
+        .forEach((oil) => {
+          const info = {
+            type: 'box',
+            layout: 'baseline',
+            contents: [
+              {
+                type: 'text',
+                text: oil.產品名稱,
+                size: 'lg',
+                flex: 2,
+              },
+              {
+                type: 'text',
+                text: `${oil.參考牌價}`,
+                flex: 1,
+                color: '#ff0000',
+                align: 'center',
+                size: 'lg',
+              },
+              {
+                type: 'text',
+                text: '元/公升',
+                flex: 1,
+                align: 'center',
+                size: 'lg',
+              },
+            ],
+            margin: 'md',
+          };
+          oilMsg.contents.body.contents.push(info);
+          oilMsg.contents.footer.contents[0].contents[1].text = `${oil.牌價生效日期}`;
+        });
+    }
 
     event.reply(oilMsg);
   } catch (error) {
